8ec047af6d6291f5a2dd1f95ba9f4b07a4481561
dianerdiana <dianerdiana.de@gmail.com>
2024-10-04 15:44:21 +0700

    Initial API

---
+++ api/src/http/middlewares/ErrorMiddleware.ts
@@ -0,0 +1,54 @@
+import { AppError } from "@libs/errors/AppError";
+import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
+import logger from "@libs/logger";
+import { Prisma } from "@prisma/client";
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export const ErrorMiddleware = (error: Error, req: Request, res: Response, next: NextFunction) => {
+  if (error instanceof ZodError) {
+    const errorMessages = error.errors.map((issue: any) => ({
+      path: issue.path[0],
+      message: issue.message,
+    }));
+
+    logger.error(errorMessages);
+    return res.status(405).json({
+      error: true,
+      status: "error",
+      message: "Invalid Input",
+      details: errorMessages,
+    });
+  }
+
+  if (error instanceof AppError) {
+    if (error.isOperational) {
+      logger.error(error.message);
+      return res.status(error.statusCode).json({
+        error: true,
+        status: "error",
+        message: error.message,
+      });
+    }
+  }
+
+  if (error instanceof Prisma.PrismaClientKnownRequestError) {
+    if (error.code === "P2002") {
+      console.error("Unique constraint error: ", error);
+
+      const fieldName = error.meta?.target;
+      return res.status(500).json({
+        error: true,
+        status: "error",
+        message: `${fieldName} is already registered.`,
+      });
+    }
+  }
+
+  logger.error(error);
+  return res.status(500).json({
+    error: true,
+    status: "error",
+    message: "An unexpected error occured.",
+  });
+};
---
+++ api/src/server.ts
@@ -0,0 +1,26 @@
+import { json, urlencoded } from "body-parser";
+import express, { type Express } from "express";
+import morgan from "morgan";
+import cors from "cors";
+import { ErrorMiddleware } from "./http/middlewares/ErrorMiddleware";
+import { ApiRoutes } from "./routes/api";
+import path from "path";
+
+export const createServer = (): Express => {
+  const app = express();
+  app
+    .disable("x-powered-by")
+    .use(morgan("dev"))
+    .use(urlencoded({ extended: true }))
+    .use(json())
+    .use(cors())
+    .use(express.static(path.join(__dirname, "public")))
+    .use(express.static(path.join(__dirname, "uploads")))
+    .get("/status", (_, res) => {
+      return res.json({ ok: true });
+    })
+    .use("/api", ApiRoutes)
+    .use(ErrorMiddleware);
+
+  return app;
+};
